5a21409708a1263861cabf4180d79e6d61ce208f
Samidore <czhang.connect@gmail.com>
2026-08-28 02:23:51 -0400

    Fix restaurant rules test assertion import

--- tests/restaurant-rules.emulator.mjs
+++ tests/restaurant-rules.emulator.mjs
@@ -1,3 +1,4 @@
+import assert from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
 import test from 'node:test';
 import { assertFails, assertSucceeds, initializeTestEnvironment } from '@firebase/rules-unit-testing';
